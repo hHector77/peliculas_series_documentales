85c21c075e6528d95165a1e5c724240cf71c90d4
hHector77 <orellanahhector@gmail.com>
2024-10-20 18:33:18 -0300

    vistasy rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  get "documentary_films/index"
-  get "documentary_films/new"
-  get "documentary_films/create"
-  get "series/index"
-  get "series/new"
-  get "series/create"
-  get "movies/index"
-  get "movies/new"
-  get "movies/create"
+  #get "documentary_films/index"
+  #get "documentary_films/new"
+  #get "documentary_films/create"
+  #get "series/index"
+  #get "series/new"
+  #get "series/create"
+  #get "movies/index"
+  #get "movies/new"
+  #get "movies/create"
+  resources :movies
+  resources :series
+  resources :documentary_films
+  root 'movies#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
